8f6590d432e9360f283e4065991d8e46e4b052ff
Hemen Kapadia <hemen.kapadia@gmail.com>
2022-05-07 16:37:53 -0700

    Virtualbox and Vagrant

--- dotfiles-store/install_script.sh
+++ dotfiles-store/install_script.sh
@@ -491,17 +491,17 @@ for choice in $choices; do
     bas120) # Installing VirtualBox
       echo ""
       echo "Installing VirtualBox...."
-      wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-      wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-      sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" -y
-      sudo apt-get update && sudo apt-get install -y virtualbox-6.0
+#      wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+#      wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+#      sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" -y
+      sudo apt-get update && sudo apt install -y virtualbox
       echo "VirtualBox installation completed."
       ;;
     bas121) # Installing Vagrant
       echo ""
       echo "Installing Vagrant ...."
-      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-      sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#      sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
       sudo apt-get update && sudo apt-get install -y vagrant
       echo "Vagrant installation completed."
       ;;
