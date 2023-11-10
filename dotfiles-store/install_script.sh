3de3670286a984f0968dedeedec75d7e00d5676d
Hemen Kapadia <hemen.kapadia@gmail.com>
2023-11-10 15:04:33 -0800

    Download latest vagrant instead from apt

--- dotfiles-store/install_script.sh
+++ dotfiles-store/install_script.sh
@@ -559,7 +559,9 @@ for choice in $choices; do
       echo "Installing Vagrant ...."
 #      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 #      sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-      sudo apt-get update && sudo apt-get install -y vagrant
+      wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+      echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+      sudo apt update && sudo apt install vagrant
       echo "Vagrant installation completed."
       ;;
     bas130) # Installing Docker Community Edition
