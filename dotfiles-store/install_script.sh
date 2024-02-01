0d7bb28039a07a753a3e03117264d4d4cbc6cb3d
Hemen Kapadia <hemen.kapadia@gmail.com>
2024-02-01 09:19:50 -0800

    Update Spotify gpgkey

--- dotfiles-store/install_script.sh
+++ dotfiles-store/install_script.sh
@@ -1294,8 +1294,8 @@ for choice in $choices; do
     med000) # Installing Spotify Client
       echo ""
       echo "Installing Spotify Client...."
-      curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify.gpg
-      echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+      curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+      echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
       sudo apt-get update && sudo apt install -y spotify-client
       echo "Spotify Client installation completed."
       echo ""
