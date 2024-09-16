e057481433318d2a433186668b659382c20627a5
Hemen Kapadia <hemen.kapadia@gmail.com>
2024-09-15 23:22:15 -0700

    Google key update

--- dotfiles-store/install_script.sh
+++ dotfiles-store/install_script.sh
@@ -637,8 +637,8 @@ for choice in $choices; do
     bas135) # Installing Google Cloud SDK
       echo ""
       echo "Installing Google Cloud SDK...."
+      curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
       echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-      curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
       sudo apt-get update && sudo apt-get install -y google-cloud-sdk
       echo "Google Cloud SDK installation completed."
       echo ""
