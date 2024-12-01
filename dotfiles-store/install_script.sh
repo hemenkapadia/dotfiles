9658f9887a7d170a85249c86fdac52f0ddb31c5e
Hemen Kapadia <hemen.kapadia@gmail.com>
2024-12-01 00:59:55 -0800

    Updating to Virtualbox 7.1

--- dotfiles-store/install_script.sh
+++ dotfiles-store/install_script.sh
@@ -561,10 +561,9 @@ for choice in $choices; do
     bas120) # Installing VirtualBox
       echo ""
       echo "Installing VirtualBox...."
-#      wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-#      wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-#      sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" -y
-      sudo apt-get update && sudo apt install -y virtualbox
+      wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+      echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+      sudo apt-get update && sudo apt install -y virtualbox-7.1
       echo "VirtualBox installation completed."
       ;;
     bas121) # Installing Vagrant
